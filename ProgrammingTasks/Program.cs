06868af5ba72baf4b72e17e7ab1eeec1fc0ea230
Daria Izviekova <daria_izviekova@epam.com>
2018-04-10 14:39:39 +0300

    Added another programming task

--- ProgrammingTasks/Program.cs
+++ ProgrammingTasks/Program.cs
@@ -10,6 +10,12 @@ namespace ProgrammingTasks
         {
             int[] nums = {2, 3, 5, 9, 4, 1, 9, 3, 6};
             PrintEvensAndOdds(nums);
+
+            Console.WriteLine("*****");
+
+            int n = 20;
+            var array = GenerateArray(n);
+            GetLongestNumbersSequence(array);
         }
 
         static void PrintEvensAndOdds(int[] array)
@@ -43,6 +49,47 @@ namespace ProgrammingTasks
             {
                 Console.WriteLine($"Index: {num.Key} - Number: {num.Value}");
             }
+        }
+
+        static void GetLongestNumbersSequence(int[] array)
+        {
+            var dict = new SortedDictionary<int, int>(Comparer<int>.Create((x, y) => y.CompareTo(x)));
+
+            for (int i = 0; i < array.Length - 1;)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] == array[j])
+                    {
+                        continue;
+                    }
+
+                    var length = j - i;
+                    if (!dict.ContainsKey(length))
+                    {
+                        dict[length] = i;
+                    }
+
+                    i = j;
+                    break;
+                }
+            }
+
+            var entry = dict.First();
+
+            Console.WriteLine($"The longest sequence of '{array[entry.Value]}'s: start index:{entry.Value} length:{entry.Key}.");
+        }
+
+        static int[] GenerateArray(int n)
+        {
+            var array = new int[n];
+            var rand = new Random(0);
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(0, 2);
+            }
+
+            return array;
         }
     }
 }
